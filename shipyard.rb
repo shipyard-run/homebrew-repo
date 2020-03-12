23e7a0d958065bf341979b9513b01b4b5b23e32f
shipyardbot <mail@shipyard.run>
2020-03-12 16:09:23 +0000

    Brew formula update for shipyard version v0.0.2

--- shipyard.rb
+++ shipyard.rb
@@ -9,15 +9,15 @@ class Shipyard < Formula
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.2/shipyard_0.0.2_Linux_x86_64.tar.gz"
-      sha256 "87921f6f3d73ba763f94d0fb95b19e5bc2355debed074bd2dc6a34dec3f806ab"
+      sha256 "004c0ec1a89d0657edf2029bb59c71cffd789183efe9b3317c4d727841693e77"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
         url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.2/shipyard_0.0.2_Linux_arm64.tar.gz"
-        sha256 "d760f7025903865f5371e804d022c5ec3ef2a2a00aa5f10ff5f35e016e10da32"
+        sha256 "4f088f9899c33f903e71f0d299efd822c7fd7d985278348b2b27e70b181215b0"
       else
         url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.2/shipyard_0.0.2_Linux_armv6.tar.gz"
-        sha256 "103fed05f5e2d5108a012a8c393a2220a7a7faabcff7fc2545600cd4b47aa1d7"
+        sha256 "4bca0eecedd97ec814e6f29c3aca58f242894a7834057ccdb9276b849ad3d97f"
       end
     end
   end
