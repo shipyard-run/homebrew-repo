21efeb0c018aadb73fbccbc7bbe89e9b1b4ae736
shipyardbot <mail@shipyard.run>
2020-04-21 12:26:02 +0100

    Brew formula update for shipyard version v0.0.19

--- shipyard.rb
+++ shipyard.rb
@@ -2,24 +2,24 @@
 class Shipyard < Formula
   desc ""
   homepage "https://shipyard.run/"
-  version "0.0.18"
+  version "0.0.19"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.18/shipyard_0.0.18_Darwin_x86_64.tar.gz"
-    sha256 "ee3f1d3bb0b9b57c4019c2532057957cd9645fe936de1ba3fec51640d06399aa"
+    url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.19/shipyard_0.0.19_Darwin_x86_64.tar.gz"
+    sha256 "9a57e9d89f933874ba718c75449d884884cd09185960d2d1ba0fd7a6c5087974"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.18/shipyard_0.0.18_Linux_x86_64.tar.gz"
-      sha256 "e3a6d6e48d3f19c01e7f70991e982a462872ca2a7a7bafe322323cea2f10c9b5"
+      url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.19/shipyard_0.0.19_Linux_x86_64.tar.gz"
+      sha256 "4b701c8509c9c456ddebc4b6e2cdf2e0c5c0857a1c40432295dc8410024f4099"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.18/shipyard_0.0.18_Linux_arm64.tar.gz"
-        sha256 "2e06f96217b7e3f894a6cd16178058f218be0bb42f64ff522769090d9db0c3c8"
+        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.19/shipyard_0.0.19_Linux_arm64.tar.gz"
+        sha256 "41250da8047c59ae5fa5e2a468f19c755a00ba0ed1f453fc8f87c2c6d05d9b3f"
       else
-        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.18/shipyard_0.0.18_Linux_armv6.tar.gz"
-        sha256 "46bfc856a6e6bd6646026710d217fc5bc2e2df6242087ca495944c27bceea681"
+        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.19/shipyard_0.0.19_Linux_armv6.tar.gz"
+        sha256 "3fff32ea9ff470e6b6724150a2f8358767c3fd4b5b0aa78c2251a94cf39ceb7a"
       end
     end
   end
