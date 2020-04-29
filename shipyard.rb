ef4a0473038b2df40646d7202a66ec0d377dabd3
shipyardbot <mail@shipyard.run>
2020-04-29 21:45:47 +0100

    Brew formula update for shipyard version v0.0.26

--- shipyard.rb
+++ shipyard.rb
@@ -2,24 +2,24 @@
 class Shipyard < Formula
   desc ""
   homepage "https://shipyard.run/"
-  version "0.0.25"
+  version "0.0.26"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.25/shipyard_0.0.25_Darwin_x86_64.tar.gz"
-    sha256 "fec13776cc4ee80e5a5ea194dbed36bf0fd1802f34f103e67be47c5e150e7942"
+    url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.26/shipyard_0.0.26_Darwin_x86_64.tar.gz"
+    sha256 "c0d6f73e67cebcbf996f7078213a9bd8a8dc6b0e4f63fc6a02486944b96bccb0"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.25/shipyard_0.0.25_Linux_x86_64.tar.gz"
-      sha256 "8a6dc5ad2fcf781d10c866217bcbfaef00e52709c722bb7209ec4dd69bfe4002"
+      url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.26/shipyard_0.0.26_Linux_x86_64.tar.gz"
+      sha256 "63019b7d08a21355eef9941bbb141f82ef58b5370b2bfbcd8c73b54ed0715f6f"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.25/shipyard_0.0.25_Linux_arm64.tar.gz"
-        sha256 "ae64508b705ff2fedfacd075bc0a2f2d6af2e7ba96564f17885b0d8fe8559713"
+        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.26/shipyard_0.0.26_Linux_arm64.tar.gz"
+        sha256 "c50fe076f4749d95ee87650939221d57475587b7bc788241c137861e75d1b686"
       else
-        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.25/shipyard_0.0.25_Linux_armv6.tar.gz"
-        sha256 "d521b3e0d0dd66ac65d58fb49b16d781d02b81ecc357cf01995b829bfd26d3ef"
+        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.26/shipyard_0.0.26_Linux_armv6.tar.gz"
+        sha256 "7239644e46976ebe231b353102b3589906d97ef7695f1ce187242abba7101c91"
       end
     end
   end
