95957e4eca08d81dd23fd9adad4d10a4b7c103db
shipyardbot <mail@shipyard.run>
2020-06-27 16:21:26 +0100

    Brew formula update for shipyard version v0.0.36

--- shipyard.rb
+++ shipyard.rb
@@ -2,24 +2,24 @@
 class Shipyard < Formula
   desc ""
   homepage "https://shipyard.run/"
-  version "0.0.35"
+  version "0.0.36"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.35/shipyard_0.0.35_Darwin_x86_64.tar.gz"
-    sha256 "0d9682b1fdde585717b1a9f3b71bb9484032038b2338918da031787dff4d6884"
+    url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.36/shipyard_0.0.36_Darwin_x86_64.tar.gz"
+    sha256 "a4d701ee88f9440f464df2939bc3e5b20aa51e776e5ee9b2056f4cdc969cf120"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.35/shipyard_0.0.35_Linux_x86_64.tar.gz"
-      sha256 "923dd310049b9f8d792e3a9da4ce91433838edf7df57e65e5ffb1bed75a7463c"
+      url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.36/shipyard_0.0.36_Linux_x86_64.tar.gz"
+      sha256 "af33304c9aead9587e6c59fe75075240591156ab6ab6a17ffd589da0df11f8e8"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.35/shipyard_0.0.35_Linux_arm64.tar.gz"
-        sha256 "51fb6c590edd3ea1f48eaebb2d46c4f94754d1a01e8eba52b86eefc0df8a48bf"
+        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.36/shipyard_0.0.36_Linux_arm64.tar.gz"
+        sha256 "1eb0da4a3f686bca459389b290ac375c49cc246c572680762d2d30b5076119d9"
       else
-        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.35/shipyard_0.0.35_Linux_armv6.tar.gz"
-        sha256 "a26b444c8cfb1b5bea9a076b36c71c52621f708b3d6f750503abe521adcbe48d"
+        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.36/shipyard_0.0.36_Linux_armv6.tar.gz"
+        sha256 "95eecd1a50b5a6fffe2fbdae83f1f3ed3c48e180fad1c11bd11725daa975e15e"
       end
     end
   end
