f7ab2696cc5a7774211b8c90dc0d216ed54a2b99
shipyardbot <mail@shipyard.run>
2020-07-04 17:57:58 +0100

    Brew formula update for shipyard version v0.0.37

--- shipyard.rb
+++ shipyard.rb
@@ -2,24 +2,24 @@
 class Shipyard < Formula
   desc ""
   homepage "https://shipyard.run/"
-  version "0.0.36"
+  version "0.0.37"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.36/shipyard_0.0.36_Darwin_x86_64.tar.gz"
-    sha256 "a4d701ee88f9440f464df2939bc3e5b20aa51e776e5ee9b2056f4cdc969cf120"
+    url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.37/shipyard_0.0.37_Darwin_x86_64.tar.gz"
+    sha256 "04101409a6e703474973f1b58501cbef3dcaef5a9cd9647c3ca0064b03907169"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.36/shipyard_0.0.36_Linux_x86_64.tar.gz"
-      sha256 "af33304c9aead9587e6c59fe75075240591156ab6ab6a17ffd589da0df11f8e8"
+      url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.37/shipyard_0.0.37_Linux_x86_64.tar.gz"
+      sha256 "42950f62ffd1e8fd8672aba1d553cf095c2074a0d910986de4a71f2efcca9e9c"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.36/shipyard_0.0.36_Linux_arm64.tar.gz"
-        sha256 "1eb0da4a3f686bca459389b290ac375c49cc246c572680762d2d30b5076119d9"
+        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.37/shipyard_0.0.37_Linux_arm64.tar.gz"
+        sha256 "66e937009ab27f30f833b59438025fba727d72f4f7f989fa1b4038051953f77a"
       else
-        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.36/shipyard_0.0.36_Linux_armv6.tar.gz"
-        sha256 "95eecd1a50b5a6fffe2fbdae83f1f3ed3c48e180fad1c11bd11725daa975e15e"
+        url "https://github.com/shipyard-run/shipyard/releases/download/v0.0.37/shipyard_0.0.37_Linux_armv6.tar.gz"
+        sha256 "a934cce603185e822678358dcb53be723d5bef9d6ccdbd09d4c066ceea1fa72b"
       end
     end
   end
